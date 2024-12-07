996d1962b212af13f2cd5066094fff09149bf1bc
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-07 15:11:01 +0530

    auth service class completed

---
+++ lib/features/auth/services/firebase_auth_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+final class FirebaseAuthService {
+  Future<void> createAccount(
+    String email,
+    String password,
+  ) async {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> sendVerificationEmail() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      await user.sendEmailVerification();
+    }
+  }
+
+  Future<void> login(
+    String email,
+    String password,
+  ) async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> logout() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
